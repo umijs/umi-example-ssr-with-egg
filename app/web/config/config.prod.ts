b70f5a7c20669852eb1f77efe24af164ef848f48
ycjcl868 <45808948@qq.com>
2019-08-06 13:58:15 +0800

    fix: react-helmet react undefined

--- app/web/config/config.prod.ts
+++ app/web/config/config.prod.ts
@@ -3,9 +3,6 @@ import { IConfig } from 'umi-types';
 export default {
   hash: true,
   publicPath: '',
-  externals: {
-    'react-helmet': 'react-helmet',
-  },
   manifest: {
     fileName: '../../config/manifest.json',
   },
